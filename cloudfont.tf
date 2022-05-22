c3d15091caad6701d1ae33e89ef8a578532373bf
Start <tart_zz@hotmail.com>
2022-05-22 18:50:32 +0000

    cloudfont update

--- cloudfont.tf
+++ cloudfont.tf
@@ -13,33 +13,26 @@ module "cdn" {
   retain_on_delete    = false
   wait_for_deployment = false
 
-  create_origin_access_identity = true
-  origin_access_identities = {
-    s3_bucket_one = "My awesome CloudFront can access"
-  }
-
-
+  create_origin_access_identity = false
+  
   origin = {
-    something = {
-      domain_name =  "${local.s3_bucket_name}.s3.amazonaws.com"
+    webELB = {
+      domain_name =  aws_lb.webLB.dns_name
+      origin_path = "/WebServerExample.php"
       custom_origin_config = {
         http_port              = 80
         https_port             = 443
-        origin_protocol_policy = "match-viewer"
+        origin_protocol_policy = "http-only"
         origin_ssl_protocols   = ["TLSv1", "TLSv1.1", "TLSv1.2"]
       }
+    
     }
 
-    s3_one = {
-      domain_name = "${local.s3_bucket_name}.s3.amazonaws.com"
-      s3_origin_config = {
-        origin_access_identity = "s3_bucket_one"
-      }
-    }
+    
   }
 
   default_cache_behavior = {
-    target_origin_id           = "something"
+    target_origin_id           = "webELB"
     viewer_protocol_policy     = "allow-all"
 
     allowed_methods = ["GET", "HEAD", "OPTIONS"]
@@ -47,24 +40,6 @@ module "cdn" {
     compress        = true
     query_string    = true
   }
-
-  ordered_cache_behavior = [
-    {
-      path_pattern           = "/static/*"
-      target_origin_id       = "s3_one"
-      viewer_protocol_policy = "redirect-to-https"
-
-      allowed_methods = ["GET", "HEAD", "OPTIONS"]
-      cached_methods  = ["GET", "HEAD"]
-      compress        = true
-      query_string    = true
-    }
-  ]
-
-#   viewer_certificate = {
-#     acm_certificate_arn = "arn:aws:acm:us-east-1:135367859851:certificate/1032b155-22da-4ae0-9f69-e206f825458b"
-#     ssl_support_method  = "sni-only"
-#   }
 
 tags = merge(local.common_tags, { Name = "${var.cName}-cdn"})
 }
